335d417a773a3f63c5689f37ebe2b8b98ab9ea30
Betty Manasse <bmanasse03@gmail.com>
2026-04-24 01:14:29 -0400

    Add headless Chrome options for Jenkins CI

--- src/test/java/com/qa/automation/utils/BaseTest.java
+++ src/test/java/com/qa/automation/utils/BaseTest.java
@@ -1,26 +1,24 @@
 package com.qa.automation.utils;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 public class BaseTest {
-
     protected WebDriver driver;
-
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
         driver.manage().timeouts()
               .implicitlyWait(java.time.Duration.ofSeconds(10));
     }
-
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
